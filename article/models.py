01a5155ea7102f2a27f34d534035612133e6a80a
jamshidi799 <jamshidi.m799@gmail.com>
2020-02-24 06:22:43 -0500

    complete article app

--- article/models.py
+++ article/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=30)
+    slug = models.SlugField(blank=True)
+    content = models.TextField(max_length=1000)
+    date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(blank=True)
+
+    def __str__(self):
+        return f'{self.title}'
--- article/views.py
+++ article/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from . import forms
+from .models import Article
 
 
 def article_list(request):
-    return HttpResponse('hello world')
+    articles = Article.objects.all().order_by('date')
+    return render(request, 'article_list.html', {'articles': articles})
+
+
+def article_detail(request, slug):
+    article = Article.objects.get(slug=slug)
+    return render(request, 'article_detail.html', {'article': article})
+
+
+def article_create(request):
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('article:list')
+    else:
+        form = forms.CreateArticle()
+    return render(request, 'new_article.html', {'form': form})
